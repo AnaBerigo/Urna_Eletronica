8d6df0a31e3fab6ebe831259bb72c246672462f8
Ana Paula Berigo e Silva <ana.berigo17@gmail.com>
2022-08-19 23:06:08 -0300

    Meu primeiro commit.

---
+++ etapas.js
@@ -0,0 +1,70 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "Fulano de Tal",
+        partido: "ABC",
+        fotos: [
+          {
+            url: "candidato3.jpg",
+            legenda: "Vereador",
+          },
+        ],
+      },
+      {
+        numero: "77222",
+        nome: "Beltrano da Silva",
+        partido: "DEFG",
+        fotos: [
+          {
+            url: "candidato6.jpg",
+            legenda: "Vereador",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Ciclano",
+        partido: "ABC",
+        vice: "Cic",
+        fotos: [
+          {
+            url: "candidato1.jpg",
+            legenda: "Prefeito",
+          },
+          {
+            url: "candidato2.jpg",
+            legenda: "Vice-Prefeito",
+            small: true,
+          },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          {
+            url: "candidato4.jpg",
+            legenda: "Prefeito",
+          },
+          {
+            url: "candidato5.jpg",
+            legenda: "Vice-Prefeito",
+            small: true,
+          },
+        ],
+      },
+    ],
+  },
+];
